bc661767bf8243c54f025095364bdef59682f707
Florian Dahlitz <f2dahlitz@freenet.de>
2019-04-16 20:10:22 +0200

    feat(build): extend the build script to properly build bison files as well

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ then
     path=$(echo "$1" | cut -f 1 -d '.')
     output_filename=$(echo ${path#*/})
     flex -o "build/$output_filename.c" "$1"
-    gcc -o "build/$output_filename.exe" -ll "build/$output_filename.c"
+    bison -d -o "build/yy_$output_filename.c" "bison/$output_filename.y"
+    gcc -o "build/$output_filename.exe" -ll -lm "build/$output_filename.c" "build/yy_$output_filename.c"
 fi
